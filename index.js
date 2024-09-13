f55ff134fd90138e5d914f180877f386cfd97154
Rafael <rafaeloliva2002@outlook.com>
2024-09-13 19:00:21 -0300

    chore: add inqurer

---
+++ index.js
@@ -0,0 +1,20 @@
+const start = () => {
+    while(true){
+        let opcao = "sair"
+       
+       
+        switch(opcao){
+            case "cadastrar":
+                console.log("CADASTRO")
+                break
+                case "listar":
+                console.log("lista")
+                break
+                case "sair":
+                return
+
+        }
+    }
+}
+
+start()
