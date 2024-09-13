96d8641e76085978bdfcd541bc6df689c3cb1720
Rafael <rafaeloliva2002@outlook.com>
2024-09-13 20:00:07 -0300

    feat: add list and and goals

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const { select } = require('@inquirer/prompts')
+const { select, input , checkbox} = require('@inquirer/prompts')
 
+let metas = []
+
+const listarMetas = async ( ) =>{
+ const result = await checkbox({
+    message:"Use as setas para navegar, o espaço para marca ou desmarcar e o Enter para finalizar.",
+    choices: [...metas],
+    instructions: false,
+ })
+    if(result.length == 0) {
+    return
+   }
+    
+   metas.forEach((m) => {
+    m.checked = false
+   })
+
+  result.forEach((resposta) => {
+   const meta = metas.find((m) => {
+          return m.value == resposta
+   })
+   meta.checked = true
+  }) 
+   console.log("Meta(s) concluída(s)!")   
+}
+
+const cadastrarMeta = async () => {
+   const meta = await input({ message: "Digite a meta:"})
+
+   if(meta.length == 0){
+   console.log("A meta não pode ser vazia.")
+   return 
+   }
+
+    metas.push({
+    value: meta, 
+    checked: false
+  })
+}
 
 const start = async () => {
     while(true){
@@ -20,11 +58,16 @@ const start = async () => {
        
         switch(opcao){
             case "cadastrar":
-                console.log("CADASTRANDO..")
+            
+               await cadastrarMeta()  
+               console.log("Cadastrado com sucesso!")
+               break
+            
+               case "listar":
+            
+                await listarMetas()
                 break
-                case "listar":
-                console.log("Listando..")
-                break
+            
                 case "sair":
                 console.log("Encerrando..")
                 return
